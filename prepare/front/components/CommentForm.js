954c8383bb4b6a813f31b17eb86a06c55a44a0f3
ohjieun <ohjieun@kakao.com>
2021-01-22 16:47:14 +0900

    댓글 구현 중

---
+++ prepare/front/components/CommentForm.js
@@ -0,0 +1,14 @@
+import React, { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'antd';
+
+
+const CommentForm = ({ post }) => {
+    return (
+        <Form>
+            폼
+        </Form>
+    )
+};
+
+export default CommentForm;
