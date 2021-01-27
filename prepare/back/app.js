25b64bd581f6c6fe4428927f91d9964f78bd3412
ohjieun <ohjieun@kakao.com>
2021-01-27 14:53:05 +0900

    express로 라우팅하기

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,13 +1,34 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.write('<h1>Hello node1</h1>');
-  res.write('<h2>Hello node2</h2>');
-  res.write('<h3>Hello node3</h3>');
-  res.write('<h4>Hello node4</h4>');
-  res.end('<h5>Hello node5</h5>');
-  // res.end는 한번만!
+const express = require('express');
+
+const app = express();
+
+// http.createServer를 구조적으로 할 수 있다.
+// app.메소드(url, (request, response) => {});
+// res.end => res.send, res.json로 사용
+app.get('/', (req, res) => {
+  res.send('hello express');
 });
-server.listen(3065, () => {
+
+app.get('/api', (req, res) => {
+  res.send('hello api');
+});
+
+app.get('/api/posts', (req, res) => {
+  res.json([
+    { id: 1, content: 'hello' },
+    { id: 2, content: 'hello2' },
+    { id: 3, content: 'hello3' },
+  ]);
+});
+
+app.post('/api/post', (req, res) => {
+  res.json({ id: 1, content: 'hello' });
+});
+
+app.delete('/api/post', (req, res) => {
+  res.json({ id: 1 });
+});
+
+app.listen(3065, () => {
   console.log('서버 실행 중');
 });
