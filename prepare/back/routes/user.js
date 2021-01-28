4767d605022d68660d0a8a3857f60d6200b7fbb6
ohjieun <ohjieun@kakao.com>
2021-01-28 15:11:12 +0900

    미들웨어로 라우터 검사하기
    middlewares.js
    중복 될 수 있는 미들웨어 생성

--- prepare/back/routes/user.js
+++ prepare/back/routes/user.js
@@ -3,11 +3,12 @@ const bcrypt = require('bcrypt');
 const passport = require('passport');
 
 const { User, Post } = require('../models');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
 // 미들웨어 확장
-router.post('/login', (req, res, next) => {
+router.post('/login', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) {
       console.error(err);
@@ -42,7 +43,7 @@ router.post('/login', (req, res, next) => {
   })(req, res, next);
 });
 
-router.post('/', async (req, res, next) => { // POST /user
+router.post('/', isNotLoggedIn, async (req, res, next) => { // POST /user
   try {
     const exUser = await User.findOne({
       where: {
@@ -67,7 +68,7 @@ router.post('/', async (req, res, next) => { // POST /user
   }
 });
 
-router.post('/user/logout', (req, res) => {
+router.post('/logout', isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
   res.send('ok');
