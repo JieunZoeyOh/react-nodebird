1b6674e59c64f0de80ea97bb68321d885a16ecaa
ohjieun <ohjieun@kakao.com>
2021-01-28 15:55:30 +0900

    게시글, 댓글 작성하기

--- prepare/back/routes/post.js
+++ prepare/back/routes/post.js
@@ -1,9 +1,42 @@
 const express = require('express');
 
+const { Post, Comment } = require('../models');
+const { isLoggedIn } = require('./middlewares');
+
 const router = express.Router();
 
-router.post('/', (req, res) => { // POST /post
-  res.json({ id: 1, content: 'hello' });
+router.post('/', isLoggedIn, async (req, res, next) => { // POST /post
+  try {
+    // create 하면 생성된 객체가 return 됨
+    const post = await Post.create({
+      content: req.body.content,
+      UserId: req.user.id, // req.user는 라우트에 접근하기 전 passport deserializeUser에서 만들어짐 
+    });
+    res.status(201).json(post); // 프론트(saga/post)에 return
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/:postId/comment', isLoggedIn, async (req, res, next) => { // POST /post/1/comment
+  try {
+    const post = await Post.findOne({
+      where: { id: req.params.postId }
+    })
+    if (!post) {
+      return res.status(403).send('존재하지 않는 게시글입니다.');
+    }
+    const comment = await Comment.create({
+      content: req.body.content,
+      PostId: req.params.postId,
+      UserId: req.user.id,
+    })
+    res.status(201).json(comment);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 router.delete('/', (req, res) => { // DELETE /post
